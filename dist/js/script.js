cfa5533010d4fe29e67fd86682aebe62e6be2f3b
konpou <kon.poulo@gmail.com>
2022-01-29 16:25:52 +0200

    Css fixes and js reveal logic

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,25 @@
-// Year
+// Year footer
 const yearNow = document.querySelector(".year");
 
 const now = new Date();
 yearNow.textContent = now.getFullYear();
+
+// Reveal projects on scroll
+const allProjects = document.querySelectorAll(".project");
+const projectReveal = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("hidden--project");
+  observer.unobserve(entry.target);
+};
+const projectObserver = new IntersectionObserver(projectReveal, {
+  root: null,
+  threshold: 0.15,
+});
+
+allProjects.forEach(function (section) {
+  projectObserver.observe(section);
+  section.classList.add("hidden--project");
+});
+// Implement reveal on scroll for form (left-right)
+// Implement lazy load images
